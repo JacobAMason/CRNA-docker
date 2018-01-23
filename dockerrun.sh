158d8525fec56d884ad7b9c14f663158a7e62afa
JacobAMason <jacob@jacobmason.net>
2018-01-23 16:12:47 -0600

    Fix host variable passing through run script
    Signed-off-by: JacobAMason <jacob@jacobmason.net>

--- dockerrun.sh
+++ dockerrun.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 REACT_APP_PATH=$1
 HOST=$(hostname --ip-address)
-docker run -it -p 19000-19001:19000-19001 -v $REACT_APP_PATH:/app -e REACT_NATIVE_PACKAGER_HOSTNAME='$HOST' jacobamason/crna
+docker run -it -p 19000-19001:19000-19001 -v $REACT_APP_PATH:/app -e REACT_NATIVE_PACKAGER_HOSTNAME=$HOST jacobamason/crna
 
